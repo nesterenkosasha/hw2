e70815a0c08b4a118b861f282454bb28fe904e13
PahaNesterenko <karandash5410@gmail.com>
2017-07-05 15:00:58 +0300

    case find

--- script.js
+++ script.js
@@ -43,6 +43,37 @@ do{
 		case "amount":{
 			alert("Amount: " + filmDatabase.length)
 		}break
+		case "find name":{
+			var foundBy = prompt("Please, enter film's name")
+			const result = []
+			if(typeof foundBy == "string"){
+				for (let f=0; f<filmDatabase.length;++f){
+					if(filmDatabase[f].name == foundBy){
+						result.push(filmDatabase[f])} }
+}
+
+
+			var message = "Search results:\n";
+			for(var i = 0; i < result.length;++i){
+				message = message + "name: " + result[i].name + ",  year: " + result[i].year;
+			}
+			alert(message);
+}break
+case "find year":{
+			var foundByY = +prompt("Please, enter film's year")
+			const result1 = []
+				for (let l=0; l<filmDatabase.length;++l){
+					if(filmDatabase[l].year == foundByY){
+						result1.push(filmDatabase[l])} }
+						var message1 = "Search results:\n";
+			for(var li = 0; li < result1.length;++li){
+				message1 = message1 + "name: " + result1[li].name + ",  year: " + result1[li].year;
+			}
+			alert(message1);
+}break
+		default:
+			alert(cmd  + " no founded")
+			break		
 	}
 }while(true)
 
