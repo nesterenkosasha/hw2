b04c9489e0098fbf63a6ec0b60125364702d0f78
PahaNesterenko <karandash5410@gmail.com>
2017-07-14 03:34:53 +0300

    filmDataBase

--- script.js
+++ script.js
@@ -1,80 +1,96 @@
-alert("Hello, welcom to film database")
+alert(hello)
 var filmDatabase = [];
 do{
-	const cmd = prompt("You can 'add', 'review','find', ask for 'help', see 'amount' of films or 'exit' ") 
+	const cmd = prompt(cmd_) 
 	if (cmd == "exit") break
 	switch(cmd){
 		case "add":{
-					var filmName = prompt("Please, enter film name")
+					var filmName = prompt(film_Name)
 					 filmN = !!filmName
 						if  (filmN==false){
 							do{
 								if (filmN == true) break
-									filmN = !!prompt("rewrite")}
+									filmN = prompt(wrong)}
 							while(true)}
 						if(filmN == true){
 								console.log(filmN)
 					}
 			
-					var filmY = +prompt("Please, enter film year")
-						 if  (filmY<1895){
-							do{
-							if (filmY >=1895) break	 		
-								filmY = prompt("rewrite")}
-								while(true)}
+					let filmY = +prompt(filmY_)
+						const da = new Date().getFullYear()
+						console.log(da, filmY)
+						do{
+						let n = filmY <= da ? (filmY>=1895?1:0):0
+						console.log(typeof n, n)
+							
+								if(n == 1)break
 
-						if(filmY >=1895){
-						console.log(filmY)	}
+							filmY = prompt(wrong)}
+					 	while(true)
+
 					const film = {
 						'name': filmName,
 						'year': filmY
 					}
 					filmDatabase.push(film); 
+
 		}break
 		case "review":{
 			var message = "";
 				for (let f=0; f<filmDatabase.length;++f){
 				message = message + "name: " + filmDatabase[f].name + ", year: " + filmDatabase[f].year + "\n";}
-				alert(message);}
-		break
-		case "help":{
-			alert("Comand 'add' can add new films in film database. \nReview - gives you opportunity to review films. \nAmount shows you amount of films.")
+				alert(message);
 		}break
+		
+		case "help":{ alert(help)
+			}break 
 		case "amount":{
 			alert("Amount: " + filmDatabase.length)
 		}break
-		case "find name":{
-			var foundBy = prompt("Please, enter film's name")
-			const result = []
-			if(typeof foundBy == "string"){
-				for (let f=0; f<filmDatabase.length;++f){
-					if(filmDatabase[f].name == foundBy){
-						result.push(filmDatabase[f])} }
+	case "find":{
+
+const searchMovie = function (data, filmDatabase) {
+  let result = []
+  for (let movie of filmDatabase) {
+    switch (data.criteria) {
+      case "year": 
+        if (movie.year == data.year) result.push(movie)
+        break
+      case "name":
+        if (movie.name == data.name) result.push(movie)
+        break
+      default:
+        result = null
+        break
+    }
+  }
+  return result
 }
 
+const showResult = function (data) {
+  const result = []
+  if (data && data.length)
+    for (let movie of data) {
+      result.push(movie.name + " " + movie.year)
+    }
+  alert("List of movies:\n" + result.join("\n"))
+  return result
+}
 
-			var message = "Search results:\n";
-			for(var i = 0; i < result.length;++i){
-				message = message + "name: " + result[i].name + ",  year: " + result[i].year;
-			}
-			alert(message);
-}break
-case "find year":{
-			var foundByY = +prompt("Please, enter film's year")
-			const result1 = []
-				for (let l=0; l<filmDatabase.length;++l){
-					if(filmDatabase[l].year == foundByY){
-						result1.push(filmDatabase[l])} }
-						var message1 = "Search results:\n";
-			for(var li = 0; li < result1.length;++li){
-				message1 = message1 + "name: " + result1[li].name + ",  year: " + result1[li].year;
-			}
-			alert(message1);
+      const criterias = {}
+      criterias["criteria"] = prompt(SEARCH_MOVIE)
+      criterias[criterias["criteria"]] = prompt(SEARCH_MOVIE_BY)
+      showResult(searchMovie(criterias, filmDatabase))
+     
+
+
 }break
 		default:
 			alert(cmd  + " no founded")
 			break		
 	}
 }while(true)
+
+
 
 
